0b99f4b36290f4bedb7ed866f2c4a64760db040e
Viktoria Streltsova <viktostrel-ipt28@lll.kpi.ua>
2026-06-16 21:27:27 +0300

    Add files via upload

---
+++ NTA_lab_3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+struct FactorBase {
+    double B;
+    vector<int> S;
+};
+
+// Sieve of Eratosthenes
+vector<int> sieve(double limit) {
+    if (limit < 2) return {};
+    int limit_int = static_cast<int>(limit);
+    vector<bool> is_prime(limit_int + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= limit_int; ++i) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= limit_int; j += i) is_prime[j] = false;
+        }
+    }
+    vector<int> primes;
+    for (int i = 2; i <= limit_int; ++i) {
+        // p < B
+        if (is_prime[i] && i < limit) primes.push_back(i);
+    }
+    return primes;
+}
+
+FactorBase build_factor_base(double n, double c = 3.38) {
+    double log_n = log(n);
+    double log_log_n = log(log_n);
+    // 0.5 * sqrt(log(n) * log(log(n)))
+    double exponent = 0.5 * sqrt(log_n * log_log_n);
+    double B = c * exp(exponent);
+    vector<int> S = sieve(B);
+    return { B, S };
+}
+
+int main()
+{
+    
+}
