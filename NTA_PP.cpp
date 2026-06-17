c6dbd7b332f306bfe8bb652cbeb93101ccc12f34
Viktoria Streltsova <viktostrel-ipt28@lll.kpi.ua>
2026-06-17 15:15:04 +0300

    Add files via upload

--- NTA_PP.cpp
+++ NTA_PP.cpp
@@ -2,10 +2,29 @@
 #include <vector>
 #include <cmath>
 #include <iomanip>
+#include <random>
+#include <algorithm>
+#include <fstream>
 using namespace std;
 
 using Vector = vector<double>;
 using Matrix = vector<Vector>;
+
+double c_norm(const Vector& v) {
+    double sum = 0.0;
+    for (double x : v) sum += x * x;
+    return sqrt(sum);
+}
+
+double get_min_row_norm(const Matrix& M) {
+    if (M.empty()) return 0.0;
+    double min_n = c_norm(M[0]);
+    for (size_t i = 1; i < M.size(); ++i) {
+        double current_norm = c_norm(M[i]);
+        if (current_norm < min_n) min_n = current_norm;
+    }
+    return min_n;
+}
 
 void basis_mu(int k, const Matrix& B, Matrix& B_star, Matrix& mu) {
     int n = B[k].size();
@@ -66,6 +85,80 @@ Matrix lll_reduction(Matrix B, double delta) {
     return B;
 }
 
+// Gauss method
+bool is_full_rank(Matrix M) {
+    int n = M.size();
+    for (int i = 0; i < n; ++i) {
+        // main element in the column i
+        int pivot = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (abs(M[j][i]) > abs(M[pivot][i])) {
+                pivot = j;
+            }
+        }
+        // (Max element -> 0), -> It isn't a full-rank mmatrix
+        if (abs(M[pivot][i]) < 1e-9) return false;
+        if (pivot != i) swap(M[i], M[pivot]);
+        for (int j = i + 1; j < n; ++j) {
+            double factor = M[j][i] / M[i][i];
+            for (int k = i; k < n; ++k) {
+                M[j][k] -= factor * M[i][k];
+            }
+        }
+    }
+    return true;
+}
+
+// Random Full-Rank matrix
+Matrix generate_rfr_matrix(int n, int min_val, int max_val, mt19937& gen) {
+    uniform_int_distribution<> distr(min_val, max_val);
+    Matrix M(n, Vector(n));
+    while (true) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) M[i][j] = distr(gen);
+        }
+        if (is_full_rank(M)) return M;
+    }
+}
+
+void matrix_to_file(ofstream& out, const Matrix& M) {
+    for (const auto& row : M) {
+        for (double val : row) out << setw(4) << static_cast<int>(val) << " ";
+        out << "\n";
+    }
+}
+
 int main() {
-    
+    random_device rd;
+    mt19937 gen(rd());
+    const int num_matrices = 50;
+    const int n = 30;
+    const int min_element = -5;
+    const int max_element = 5;
+    const double delta = 0.75;
+    ofstream out_file("lll_50_matrixes.txt");
+    if (!out_file.is_open()) {
+        cerr << "ERROR!!! Cannot open the file." << std::endl;
+        return 1;
+    }
+    out_file << "Results for the " << num_matrices << " random matrices (" << n << "x" << n << ")\n";
+    out_file << "delta = " << delta << "\n";
+    for (int i = 1; i <= num_matrices; ++i) {
+        Matrix B = generate_rfr_matrix(n, min_element, max_element, gen);
+        double min_norm_before = get_min_row_norm(B);
+        Matrix reduced_B = lll_reduction(B, delta);
+        double min_norm_after = get_min_row_norm(reduced_B);
+        out_file << "Matrix #" << i << "\n";
+        out_file << "Min norm before hte reduction:   " << fixed << setprecision(4) << min_norm_before << "\n";
+        out_file << "Min norm after the reduction: " << min_norm_after << "\n\n";
+        out_file << "Basis matrix:\n";
+        matrix_to_file(out_file, B);
+        out_file << "\n";
+        out_file << "LLL-reduced basis:\n";
+        matrix_to_file(out_file, reduced_B);
+        out_file << "\n\n";
+    }
+    out_file.close();
+    cout << "\nResults are in the file lll_50_matrixes.txt" << endl;
+    return 0;
 }
